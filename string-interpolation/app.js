19584a6a2c259757d518dbd94f6b9e573a979a31
Jan Wojcik <123651940+Janowoj@users.noreply.github.com>
2024-01-09 15:41:49 +0100

    added tagged templates to the app.js

--- string-interpolation/app.js
+++ string-interpolation/app.js
@@ -14,4 +14,15 @@ const longText = ` Lorem ipsum, dolor sit amet
  consectetur adipisicing elit. Aspernatur inventore reiciendis maiores suscipit adipisci quam asperiores ipsa deleniti? Eligendi quae tempora reiciendis, quis sunt recusandae suscipit quaerat ipsam autem error.
 Eum quas eos cum iusto ducimus, esse hic quos molestias repellendus ad perspiciatis ratione, voluptas unde, quae animi harum itaque! Ipsum tempora cum, ipsam officiis molestiae accusantium? Tempora, excepturi esse.`;
 
+// tagged template literals
 console.log`Hello, my name is ${myName}.`;
+
+function tag(strings, ...values) {
+  console.log(strings);
+  console.log(values);
+}
+
+tag`Hello, my name is ${myName}.`;
+// [ 'Hello, my name is ', '.' ]
+
+// [ 'John Doe' ]
